e3293649da75e524e4fae9d05b51cc23eb7f3db7
[jsw324] <jwalkow@tallience.com>
2017-05-25 16:01:28 -0400

    Added ability to run tests with karma and mocha

---
+++ app/tests/components/AddPerm.test.jsx
@@ -0,0 +1,14 @@
+var expect = require('expect');
+var React = require('react');
+var ReactDOM = require('react-dom');
+var TestUtils = require('react-dom/test-utils');
+var $ = require('jquery');
+
+import * as actions from 'actions';
+import AddPerm from 'AddPerm';
+
+describe('AddPerm', () => {
+  it('should run test', () => {
+    expect('test').toEqual('test');
+  })
+});
